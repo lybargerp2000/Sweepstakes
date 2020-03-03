b517ef9149e26da8430e93105eb01ce493d5d087
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-03-03 10:58:40 -0600

    Building QueueManager class.

--- SweepStakes_1/ISweepstakesManager.cs
+++ SweepStakes_1/ISweepstakesManager.cs
@@ -8,7 +8,7 @@ namespace SweepStakes_1
 {
     public interface ISweepstakesManager
     {
-        void InsertSweepstakes(Sweepstakes sweepstakes);
+        void InsertSweepstakes();
 
 
         void SweepstakesGetSweepstakes();
--- SweepStakes_1/SweepstakesStackManager.cs
+++ SweepStakes_1/SweepstakesStackManager.cs
@@ -9,31 +9,37 @@ namespace SweepStakes_1
     public class SweepstakesStackManager : ISweepstakesManager
     {
         int numberOfSweepstakes;
+        string ChoosenSweepstakes;
+        Stack<Sweepstakes> stack;
         //string nameOfSweepstakes;
      public void InsertSweepstakes(Sweepstakes sweepstakes)
         {
-            Stack<string> stack = new Stack<string>();
-            stack.Push(sweepstakes.nameOfSweepstakes);
+            
+            stack.Push(sweepstakes);
             numberOfSweepstakes++;
 
-            Console.WriteLine("Elements in stack: ");
-            foreach (string name in stack)
-            {
-                Console.WriteLine(name);
-            }
-            string topOfStack = stack.Pop();
-            stack.Push(sweepstakes.nameOfSweepstakes);
+            //Console.WriteLine("Elements in stack: ");
+            //foreach (string name in stack)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            //string topOfStack = stack.Pop();
+            //stack.Push(sweepstakes.nameOfSweepstakes);
 
-            Console.WriteLine("Elements in stack after Pop and Push(nameOfSweepstakes): ");
-            foreach (string name in stack)
-            {
-                Console.WriteLine(name);
-            }
+            //Console.WriteLine("Elements in stack after Pop and Push(nameOfSweepstakes): ");
+            //foreach (string name in stack)
+            //{
+            //    Console.WriteLine(name);
+            //    return;
+            //}
            
         }
 
-     public void SweepstakesGetSweepstakes()
+     public   Sweepstakes GetSweepstakes()
         {
+
+            return stack.Pop();
+            
 
         }
 
